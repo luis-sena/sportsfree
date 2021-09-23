4ed979da6208e61306c436975afa5c690fbfd909
Luis Sena <luisfj.sena@gmail.com>
2021-09-23 20:08:42 -0300

    Commit inicial

---
+++ src/main/java/br/com/lsena/sportsfree/SportsFreeApplication.java
@@ -0,0 +1,13 @@
+package br.com.lsena.sportsfree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsFreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsFreeApplication.class, args);
+    }
+
+}
