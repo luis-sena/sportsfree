7ac0562a97c9eda1b2e3a8e74b73616dd7c72973
luis-sena <82774448+luis-sena@users.noreply.github.com>
2021-11-04 22:46:06 -0300

    Adicionando Cors (#7)
    * Adicionando Cors
    
    * Adicionando Cors

--- src/main/java/br/com/sportsfree/config/CorsConfiguration.java
+++ src/main/java/br/com/sportsfree/config/CorsConfiguration.java
@@ -10,12 +10,6 @@ public class CorsConfiguration implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:[*]");
-
-        registry.addMapping("/**")
-                .allowedOriginPatterns("http://127.0.0.1:[*]");
-
-        registry.addMapping("/**")
-                .allowedOriginPatterns("https://front-bootcamp.herokuapp.com");
+                .allowedOriginPatterns("http://localhost:[*]", "https://front-bootcamp.herokuapp.com", "http://127.0.0.1:[*]");
     }
 }
