3711b2dd22c8bda3a51caa5adacdfe3d3f7c1192
Luis Sena <luisfj.sena@gmail.com>
2022-01-18 11:37:11 -0300

    Adicionando validacao e email invalido

--- src/main/java/br/com/sportsfree/dto/ProfessorDto.java
+++ src/main/java/br/com/sportsfree/dto/ProfessorDto.java
@@ -3,14 +3,18 @@ package br.com.sportsfree.dto;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
+import javax.validation.constraints.Email;
+
 @Getter
 @Setter
 @SuperBuilder
 @EqualsAndHashCode(callSuper=false)
 @NoArgsConstructor
 @AllArgsConstructor
-public class ProfessorDto extends AbstractDto{
+public class ProfessorDto extends AbstractDto {
+
     private String nome;
+    @Email(message = "Email invalido", regexp = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$")
     private String email;
     private String cpf;
     private String rg;
